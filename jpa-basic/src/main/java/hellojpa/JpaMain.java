5ecf56d3c89018b228cbb880777a0a8ff9f36809
JunHaeng2 <gjwnsgod321@gmail.com>
2022-04-24 12:35:12 +0900

    0424 study

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -7,23 +7,6 @@ import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        EntityManager em= emf.createEntityManager();
-        // code
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        try {
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.getId() = " + findMember.getId());
-
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-        emf.close();
     }
 }
