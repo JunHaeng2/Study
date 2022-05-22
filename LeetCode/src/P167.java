47eed2a0e75e90c49e01e99656f599c6250f857f
JunHaeng2 <gjwnsgod321@gmail.com>
2022-05-22 20:15:19 +0900

    0522 LeetCode

---
+++ LeetCode/src/P167.java
@@ -0,0 +1,15 @@
+public class P167 {
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int start = 0, end = numbers.length - 1;
+
+            while (start < end) {
+                if (target == numbers[start] + numbers[end]) break;
+                if (target > numbers[start] + numbers[end]) start++;
+                else end--;
+            }
+
+            return new int[]{start+1, end+1};
+        }
+    }
+}
