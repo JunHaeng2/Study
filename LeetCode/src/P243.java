986bbbc9f181f66b482843229cef5532a92cf06d
JunHaeng2 <gjwnsgod321@gmail.com>
2022-05-22 21:19:00 +0900

    0522 LeetCode

---
+++ LeetCode/src/P243.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class P243 {
+    class Solution {
+        public int shortestDistance(String[] wordsDict, String word1, String word2) {
+            List<Integer> arr1 = new ArrayList<>();
+            List<Integer> arr2 = new ArrayList<>();
+
+            int ans = Integer.MAX_VALUE;
+            for (int i = 0; i < wordsDict.length; i++) {
+                if (wordsDict[i].equals(word1)) arr1.add(i);
+                else if (wordsDict[i].equals(word2)) arr2.add(i);
+            }
+
+            for (int i = 0; i < arr1.size(); i++) {
+                for (int j = 0; j < arr2.size(); j++) {
+                    ans = Math.min(ans, Math.abs(arr1.get(i) - arr2.get(j)));
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ LeetCode/src/P252.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class P252 {
+    class Solution {
+        public boolean canAttendMeetings(int[][] arr) {
+
+            Arrays.sort(arr, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    if (o1[0] == o2[0]) return o1[1] - o2[1];
+                    else return o1[0] - o2[0];
+                }
+            });
+
+            for (int start = 0; start < arr.length - 1; start++) {
+                if (arr[start+1][0] < arr[start][1]) return false;
+            }
+
+            return true;
+        }
+    }
+}
