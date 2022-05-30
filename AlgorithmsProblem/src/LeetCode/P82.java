03b59e5f5896b8c40ac29d140d9c32dde264ad22
JunHaeng2 <gjwnsgod321@gmail.com>
2022-05-30 21:15:46 +0900

    0530 알고리즘 공부

---
+++ AlgorithmsProblem/src/LeetCode/P82.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+public class P82 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+//    class Solution {
+//        public ListNode deleteDuplicates(ListNode head) {
+//            Map<Integer, Integer> map = new HashMap<>();
+//            ListNode curr = head;
+//
+//            while (curr != null) {
+//                map.put(curr.val, map.getOrDefault(curr.val, 0) + 1);
+//                curr = curr.next;
+//            }
+//
+//            ListNode dummy = new ListNode(0);
+//            ListNode tail = dummy;
+//
+//            while (head != null) {
+//                if (map.get(head.val) == 1) {
+//                    tail.next = head;
+//                    tail = tail.next;
+//                }
+//                head = head.next;
+//            }
+//
+//            tail.next = null;
+//            return dummy.next;
+//        }
+//    }
+}
